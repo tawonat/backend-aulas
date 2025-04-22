73db51287dc62e9ce373844fc459d8f936b9fb88
tawonat <taloswi@gmail.com>
2025-04-22 08:39:16 -0300

    deletando funções desnecessárias

--- Aula 3/userservice.js
+++ Aula 3/userservice.js
@@ -5,43 +5,6 @@ const bcrypt = require('bcryptjs') //bcrypt é uma biblioteca para criptografar 
 const mysql = require("./mysql")
 
 class userService {
-  constructor() { //quando não passa parâmetro traz um valor fixo, que não muda
-    this.filePath = path.join(__dirname, 'users.json') //a biblioteca path vem de um caminho, puca um diretorio virtual (dirname) e puxa o arquivo users.json
-    this.users = this.loadUsers()
-    this.nextID = this.getNextID()
-  }
-
-  loadUsers() { //função pra carregar usuários
-    try { //vai tentar isso enquanto der certo
-      if (fs.existsSync(this.filePath)) { //se dentro do arquivo Json existir algum dado, ele da true e entra no if
-        const data = fs.readFileSync(this.filePath) //quando true, ele da um read, ele lê o arquivo Json e salva na Data
-        return JSON.parse(data) //ele transforma a data do json em um Array de objetos
-      }
-    } catch (erro) { //se o try der errado, ele vai mostrar que deu erro
-      console.log('Erro ao carregar arquivo', erro) //vai ler o erro que deu no console
-    }
-    return [] //se não der true, roda um array vazio
-  }
-
-
-  //definir o proximo id a ser utilizado
-  getNextID() { //função para buscar o proximo id
-    try {
-      if (this.users.length === 0) return 1//se eu tiver um user dentro, ele não vai entrar, se tiver com 0 usuários ele entra e coloca ID 1
-      return Math.max(...this.users.map(user => user.id)) + 1 //mostra pra gente o maior valor possível de um ID, ai ele mapeia o o ultimo user, e se o proximo for maior (vai ser maior por ser o próximo]) ele adiciona +1 no ID
-    } catch (erro) {
-      console.log('Erro ao buscar próximo ID', erro)
-    }
-  }
-
-  saveUsers() { //função pra salvar usuários
-    try {
-      fs.writeFileSync(this.filePath, JSON.stringify(this.users)) //writefile salva o arquivo, e depois eu mostro pra ele onde está o arquivo ou seja "Vai salvar aquivo no filepath em formato json"
-    } catch (erro) {
-      console.log('Erro ao salvar arquivo', erro)
-    }
-  }
-
 
   async addUser(nome, email, senha, endereco, telefone, cpf) { //função pra adicionar usuários (é async pq ela espera algo acontecer pra continuar)
     try {
@@ -84,8 +47,6 @@ class userService {
       console.log('Erro ao deletar usuário', erro) //me avisa se der erro
     }
   }
-
-
 
  async putUser(id, nome, email, senha, endereço, telefone, cpf) {
     try {
